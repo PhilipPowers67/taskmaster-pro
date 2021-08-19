95de1b4c85d85d5ad6dccfbd62e9c0069d8a8464
Philip Powers <philip_powers607@hotmail.com>
2021-08-19 10:35:06 -0400

    fixing code

--- assets/js/script.js
+++ assets/js/script.js
@@ -3,7 +3,6 @@ var tasks = {};
 var createTask = function(taskText, taskDate, taskList) {
   // create elements that make up a task item
   var taskLi = $("<li>").addClass("list-group-item");
-
   var taskSpan = $("<span>")
     .addClass("badge badge-primary badge-pill")
     .text(taskDate);
@@ -13,7 +12,6 @@ var createTask = function(taskText, taskDate, taskList) {
 
   // append span and p element to parent li
   taskLi.append(taskSpan, taskP);
-
 
   // append to ul list on the page
   $("#list-" + taskList).append(taskLi);
@@ -44,6 +42,57 @@ var loadTasks = function() {
 var saveTasks = function() {
   localStorage.setItem("tasks", JSON.stringify(tasks));
 };
+
+$(".card .list-group").sortable( {
+  connectWith: $(".card .list-group"),
+  scroll: false,
+  tolerance: "pointer",
+  helper: "clone",
+  activate: function(event, ui) {
+    // console.log("activate", this);
+  },
+  deactivate: function(event, ui) {
+    // console.log("deactivate", this);
+  },
+  over: function(event) {
+    // console.log("over", event.target);
+  },
+  out: function(event) {
+    // console.log("out", event.target);
+  },
+  update: function(event) {
+    var tempArr = [];
+
+    // loop over current set of children in sortable list
+    $(this)
+      .children()
+      .each(function() {
+        tempArr.push({
+          text = $(this)
+            .find("p")
+            .text()
+            .trim(),
+          date: $(this)
+            .find("span")
+            .text()
+            .trim()
+         }); 
+       });  
+
+    // trim down list's ID to match object property
+    var arrName = $(this)
+      .attr("id")
+      .replace("list-", "");
+
+      // update array on tasks object and save
+      tasks[arrName] = tempArr;
+      saveTasks();
+  },
+  stop: function(event) {
+    $(this).removeClass("dropover");
+  }
+}
+);
 
 $(".list-group").on("click", "p", function() {
   var text = $(this)
@@ -131,56 +180,7 @@ $(".list-group").on("blur", "input[type='text']", function() {
   $(this).replaceWith(taskSpan);
 });
 
-$(".card .list-group").sortable( {
-  connectWith: $(".card .list-group"),
-  scroll: false,
-  tolerance: "pointer",
-  helper: "clone",
-  activate: function(event) {
-    // console.log("activate", this);
-  },
-  deactivate: function(event) {
-    // console.log("deactivate", this);
-  },
-  over: function(event) {
-    // console.log("over", event.target);
-  },
-  out: function(event) {
-    // console.log("out", event.target);
-  },
-  update: function(event) {
-    var tempArr = [];
 
-    // loop over current set of children in sortable list
-    $(this).children().each(function() {
-      var text = $(this)
-      .find("p")
-      .text()
-      .trim();
-
-      var date = $(this)
-        .find("span")
-        .text()
-        .trim()
-        
-        // add task data to the temp array as an object
-        tempArr.push({
-          text: text,
-          date: date
-        });
-    });
-    
-    // trim down list's ID to match object property
-    var arrName = $(this)
-      .attr("id")
-      .replace("list-", "");
-
-      // update array on tasks object and save
-      tasks[arrName] = tempArr;
-      saveTasks();
-  }
-}
-);
 
 $("#trash").droppable({
   accept: ".card .list-group-item",
